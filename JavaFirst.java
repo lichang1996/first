fdea0c46ee3bb0d4827e5b2216fd3f99308ee50f
li <916429424@qq.com>
2018-01-02 20:00:42 +0800

    添加了 变量a

--- JavaFirst.java
+++ JavaFirst.java
@@ -3,9 +3,12 @@ package com.subl.first;
 import org.junit.Test;
 
 public class JavaFirst {
-
+	
+	private String a;
 	@Test
 	public void testHelloWrold() {
 		System.out.println("HelloWorld!");
+
+		
 	}
 }
